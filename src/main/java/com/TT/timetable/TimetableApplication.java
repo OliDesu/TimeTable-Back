6ce8db776f1cb9c7d76680e9144a91a388ee7edc
Ali <46926472+OliDesu@users.noreply.github.com>
2023-05-16 01:08:53 +0200

    Adding front

---
+++ src/main/java/com/TT/timetable/TimetableApplication.java
@@ -0,0 +1,13 @@
+package com.TT.timetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableApplication.class, args);
+	}
+
+}
